c4c890dc409f5e2e972126392d706c42b47f22bb
chobojai123 <davidcheng1290@gmail.com>
2018-03-06 16:27:19 -0800

    updated part 2

--- src/underbar.js
+++ src/underbar.js
@@ -205,12 +205,26 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(accumulator, item){
+      if(!iterator(item)) {
+        return false;
+      } else {
+        return accumulator;
+      }
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    return (!_.every(collection, function(item) {
+      if(!iterator(item)){
+        return true;
+      }
+    }))
   };
 
 
@@ -233,11 +247,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for(var i = 0; i < arguments.length; i++){
+      for(var key in arguments[i]){
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for(var i = 0; i < arguments.length; i ++){
+      for(var key in arguments[i]){
+        if(!(key in obj)){
+          obj[key] = arguments[i][key];
+        }
+      }
+    }
+    return obj;
   };
 
 
@@ -281,6 +309,14 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var result = {};
+    return function(){
+      var functionString = JSON.stringify(arguments);
+      if(!(functionString in result)){
+        result[functionString] = func.apply(this, arguments);
+      }
+      return result[functionString];
+    }
   };
 
   // Delays a function for the given number of milliseconds, and then calls
